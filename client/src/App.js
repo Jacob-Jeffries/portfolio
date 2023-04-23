176c9445d8be180123fdfd155ff77ceabedaf000
Jacob Jeffries <jeffrij85@gmail.com>
2023-04-23 17:47:26 -0400

    changed icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Card from './components/Card';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div>
       <Header />
-      <Navbar />
       <Card />
       <Card />
       <Card />
